524c58e63b24eb50db29ec5f5021237451a8adc6
Projects <artur_kulembetov@outlook.com>
2024-05-06 11:03:40 +0700

    implement tertiary, destructive, link-gray and link-color button types

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import { StarIcon } from "./components/StarIcon/StarIcon";
 
 function App() {
   return (
-    <div className="flex flex-col gap-12">
-      <div className="max-w-full flex flex-wrap gap-5">
+    <div className="flex flex-col gap-12 mt-[7rem] mb-[7rem]">
+      <div className="max-w-full flex flex-wrap gap-5 items-center">
         <Button
           type="primary"
           size="medium"
@@ -16,7 +16,7 @@ function App() {
           to="/"
           disabled={true}
         >
-          Medium
+          Disabled
         </Button>
         <Button
           type="primary"
@@ -74,7 +74,8 @@ function App() {
           iconOnly={true}
         />
       </div>
-      <div className="max-w-full flex flex-wrap gap-5">
+
+      <div className="max-w-full flex flex-wrap gap-5 items-center">
         <Button
           type="secondary"
           size="medium"
@@ -83,7 +84,7 @@ function App() {
           to="/"
           disabled={true}
         >
-          Medium
+          Disabled
         </Button>
         <Button
           type="secondary"
@@ -143,6 +144,248 @@ function App() {
           type="secondary"
           size="extra-large-2"
           iconColor="#171717"
+          iconCenter={<StarIcon />}
+          to="/"
+          iconOnly={true}
+        />
+      </div>
+
+      <div className="max-w-full flex flex-wrap gap-5 items-center">
+        <Button
+          type="tertiary"
+          size="medium"
+          iconBefore={<StarIcon />}
+          iconAfter={<StarIcon />}
+          to="/"
+          disabled={true}
+        >
+          Disabled
+        </Button>
+        <Button
+          type="tertiary"
+          size="large"
+          iconColor="#4338CA"
+          iconBefore={<StarIcon />}
+          iconAfter={<StarIcon />}
+          to="/"
+        >
+          Large
+        </Button>
+        <Button
+          type="tertiary"
+          size="extra-large"
+          iconColor="#4338CA"
+          iconBefore={<StarIcon />}
+          iconAfter={<StarIcon />}
+          to="/"
+        >
+          Extra large
+        </Button>
+        <Button
+          type="tertiary"
+          size="extra-large-2"
+          iconColor="#4338CA"
+          iconBefore={<StarIcon />}
+          iconAfter={<StarIcon />}
+          to="/"
+        >
+          2 Extra large
+        </Button>
+        <Button
+          type="tertiary"
+          size="medium"
+          iconColor="#4338CA"
+          iconCenter={<StarIcon />}
+          to="/"
+          iconOnly={true}
+        />
+        <Button
+          type="tertiary"
+          size="large"
+          iconColor="#4338CA"
+          iconCenter={<StarIcon />}
+          to="/"
+          iconOnly={true}
+        />
+        <Button
+          type="tertiary"
+          size="extra-large"
+          iconColor="#4338CA"
+          iconCenter={<StarIcon />}
+          to="/"
+          iconOnly={true}
+        />
+        <Button
+          type="tertiary"
+          size="extra-large-2"
+          iconColor="#4338CA"
+          iconCenter={<StarIcon />}
+          to="/"
+          iconOnly={true}
+        />
+      </div>
+
+      <div className="max-w-full flex flex-wrap gap-5 items-center">
+        <Button
+          type="link-color"
+          size="medium"
+          iconBefore={<StarIcon />}
+          iconAfter={<StarIcon />}
+          to="/"
+          disabled={true}
+        >
+          Disabled
+        </Button>
+        <Button
+          type="link-color"
+          size="large"
+          iconColor="#4338CA"
+          hoverColor="#3730A3"
+          iconBefore={<StarIcon />}
+          iconAfter={<StarIcon />}
+          to="/"
+        >
+          Large
+        </Button>
+        <Button
+          type="link-color"
+          size="extra-large"
+          iconColor="#4338CA"
+          hoverColor="#3730A3"
+          iconBefore={<StarIcon />}
+          iconAfter={<StarIcon />}
+          to="/"
+        >
+          Extra large
+        </Button>
+        <Button
+          type="link-color"
+          size="extra-large-2"
+          iconColor="#4338CA"
+          hoverColor="#3730A3"
+          iconBefore={<StarIcon />}
+          iconAfter={<StarIcon />}
+          to="/"
+        >
+          2 Extra large
+        </Button>
+      </div>
+
+      <div className="max-w-full flex flex-wrap gap-5 items-center">
+        <Button
+          type="link-gray"
+          size="medium"
+          iconBefore={<StarIcon />}
+          iconAfter={<StarIcon />}
+          to="/"
+          disabled={true}
+        >
+          Disabled
+        </Button>
+        <Button
+          type="link-gray"
+          size="large"
+          iconColor="#525252"
+          hoverColor="#171717"
+          iconBefore={<StarIcon />}
+          iconAfter={<StarIcon />}
+          to="/"
+        >
+          Large
+        </Button>
+        <Button
+          type="link-gray"
+          size="extra-large"
+          iconColor="#525252"
+          hoverColor="#171717"
+          iconBefore={<StarIcon />}
+          iconAfter={<StarIcon />}
+          to="/"
+        >
+          Extra large
+        </Button>
+        <Button
+          type="link-gray"
+          size="extra-large-2"
+          iconColor="#525252"
+          hoverColor="#171717"
+          iconBefore={<StarIcon />}
+          iconAfter={<StarIcon />}
+          to="/"
+        >
+          2 Extra large
+        </Button>
+      </div>
+
+      <div className="max-w-full flex flex-wrap gap-5 items-center">
+        <Button
+          type="destructive"
+          size="medium"
+          iconBefore={<StarIcon />}
+          iconAfter={<StarIcon />}
+          to="/"
+          disabled={true}
+        >
+          Disabled
+        </Button>
+        <Button
+          type="destructive"
+          size="large"
+          iconColor="#fff"
+          iconBefore={<StarIcon />}
+          iconAfter={<StarIcon />}
+          to="/"
+        >
+          Large
+        </Button>
+        <Button
+          type="destructive"
+          size="extra-large"
+          iconColor="#fff"
+          iconBefore={<StarIcon />}
+          iconAfter={<StarIcon />}
+          to="/"
+        >
+          Extra large
+        </Button>
+        <Button
+          type="destructive"
+          size="extra-large-2"
+          iconColor="#fff"
+          iconBefore={<StarIcon />}
+          iconAfter={<StarIcon />}
+          to="/"
+        >
+          2 Extra large
+        </Button>
+        <Button
+          type="destructive"
+          size="medium"
+          iconColor="#fff"
+          iconCenter={<StarIcon />}
+          to="/"
+          iconOnly={true}
+        />
+        <Button
+          type="destructive"
+          size="large"
+          iconColor="#fff"
+          iconCenter={<StarIcon />}
+          to="/"
+          iconOnly={true}
+        />
+        <Button
+          type="destructive"
+          size="extra-large"
+          iconColor="#fff"
+          iconCenter={<StarIcon />}
+          to="/"
+          iconOnly={true}
+        />
+        <Button
+          type="destructive"
+          size="extra-large-2"
+          iconColor="#fff"
           iconCenter={<StarIcon />}
           to="/"
           iconOnly={true}
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Button.module.css";
 import { useNavigate } from "react-router-dom";
 
@@ -13,32 +13,36 @@ export const Button = ({
   iconAfter,
   iconCenter,
   iconOnly,
+  hoverColor = iconColor,
   to,
 }) => {
+  const [isHovered, setIsHovered] = useState(false);
   const navigate = useNavigate();
+
   const handleClick = () => {
-    if (disabled) {
-      return;
-    }
-
-    if (to) {
-      navigate(to);
-    }
-
-    if (onClick) {
-      onClick();
-    }
+    if (disabled) return;
+    if (to) navigate(to);
+    if (onClick) onClick();
   };
 
   const disabledColor = "#A3A3A3";
-  const buttonClass = `${styles.button} ${styles[type]} ${styles["size-" + size]} ${iconOnly ? styles[`size-${size}-iconOnly`] : ""} ${disabled ? styles.disabled : ""}`;
 
   return (
-    <button className={buttonClass} disabled={disabled} onClick={handleClick}>
+    <button
+      className={`${styles.button} ${styles[type]} ${styles["size-" + size]} ${iconOnly ? styles[`size-${size}-iconOnly`] : ""} ${disabled ? styles.disabled : ""}`}
+      disabled={disabled}
+      onClick={handleClick}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       {iconBefore && (
         <span className={styles.icon}>
           {React.cloneElement(iconBefore, {
-            color: disabled ? disabledColor : iconColor,
+            color: disabled
+              ? disabledColor
+              : isHovered
+                ? hoverColor
+                : iconColor,
           })}
         </span>
       )}
@@ -46,14 +50,22 @@ export const Button = ({
       {iconCenter && (
         <span className={styles.icon}>
           {React.cloneElement(iconCenter, {
-            color: disabled ? disabledColor : iconColor,
+            color: disabled
+              ? disabledColor
+              : isHovered
+                ? hoverColor
+                : iconColor,
           })}
         </span>
       )}
       {iconAfter && (
         <span className={styles.icon}>
           {React.cloneElement(iconAfter, {
-            color: disabled ? disabledColor : iconColor,
+            color: disabled
+              ? disabledColor
+              : isHovered
+                ? hoverColor
+                : iconColor,
           })}
         </span>
       )}
